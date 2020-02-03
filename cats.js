9935abe1bf1b59168d5a2486681705098e5b9392
Vellyus <53130942+Vellyus@users.noreply.github.com>
2020-02-04 00:48:31 +0100

    Add files via upload

--- cats.js
+++ cats.js
@@ -1,5 +1,6 @@
 const numOfCats = 27;
 let cats = [];
+let imgs = [];
 
 class Cat {
   constructor() {
@@ -16,4 +17,48 @@ function getCats() {
 }
 
 getCats();
+let random;
 
+for (let i = 0; i < 12; i++) {
+  do {
+    random = Math.floor(Math.random() * 27);
+  } while (cats[random].unused === false);
+
+  imgs[i] = document.createElement('img');
+  imgs[i].setAttribute('src', cats[random].src);
+  imgs[i].style.width = '350px';
+  imgs[i].style.margin = '20px';
+  imgs[i].style.boxSizing = 'border-box';
+  imgs[i].style.border = 'solid lightgrey 1px';
+  imgs[i].style.boxShadow = '8px 8px 16px lightgrey';
+  imgs[i].style.cursor = 'pointer';
+  imgs[i].addEventListener('onmousemove', () => { console.log('moew') });
+  document.querySelector('body').style.textAlign = 'center';
+  document.querySelector('body').appendChild(imgs[i]);
+  if (cats[random].unused === true) {
+    cats[random].unused = false;
+  }
+}
+
+const audio = document.createElement('audio');
+audio.style.display = 'none';
+document.querySelector('body').appendChild(audio);
+
+for (i = 0; i < imgs.length; i++) {
+  imgs[i].addEventListener('click', () => {
+    audio.src = "audio/meow.mp3";
+    audio.play();
+  })
+  imgs[i].addEventListener('mouseenter', () => {
+    setTimeout(() => {
+      audio.src = "audio/purr.mp3";
+      // audio.setAttribute('loop', true);
+      audio.play();
+    }, 3000)
+  })
+  imgs[i].addEventListener('mouseout', () => {
+    setTimeout(() => {
+      audio.pause();
+    }, 3000)
+  })
+}
